70b7ad428b54498a731ee31fe25f19acc1f5d7ee
elisa-daria <elisa.daria.tomasi@gmail.com>
2024-04-09 15:14:37 +0200

    adds dependencies

---
+++ src/main/java/elisadaria/UN5W2d2praticaS2L2/Un5W2D2PraticaS2L2Application.java
@@ -0,0 +1,13 @@
+package elisadaria.UN5W2d2praticaS2L2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Un5W2D2PraticaS2L2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Un5W2D2PraticaS2L2Application.class, args);
+	}
+
+}
---
+++ src/test/java/elisadaria/UN5W2d2praticaS2L2/Un5W2D2PraticaS2L2ApplicationTests.java
@@ -0,0 +1,13 @@
+package elisadaria.UN5W2d2praticaS2L2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Un5W2D2PraticaS2L2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
